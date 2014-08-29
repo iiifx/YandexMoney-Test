3b246491eb68fc4744b0e598b8f4ed30cf667f14
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2014-08-29 17:58:01 +0300

    no message

--- do/oauth_request.php
+++ do/oauth_request.php
@@ -1,16 +1,24 @@
 <?php
 
+echo 1;
+
 define( 'BASEDIR', realpath( __DIR__ . '/../' ) );
+
+echo 2;
 
 # Подключаем модули Компосера
 require_once BASEDIR . 'composer/vendor/autoload.php';
 
+echo 3;
+
 # Подключаем настройки кошелька Яндекс.Деньги
 require_once BASEDIR . 'options/seller.php';
+
+echo 4;
 
 echo '<pre>';
 var_export( $_POST );
 
-
+echo 5;
 
 
