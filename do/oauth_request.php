77c09a2733a71240d12f2304758a00670fd64e7c
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2014-08-29 17:45:39 +0300

    no message

--- do/oauth_request.php
+++ do/oauth_request.php
@@ -1,6 +1,10 @@
 <?php
 
+# Подключаем модули Компосера
 require_once __DIR__ . '/../composer/vendor/autoload.php';
+
+# Подключаем настройки кошелька Яндекс.Деньги
+require_once __DIR__ . '/../options/seller.php';
 
 echo '<pre>';
 var_export( $_POST );
---
+++ options/seller.php
@@ -0,0 +1,8 @@
+<?php
+
+define( 'YM_PURSE', '410012472338652' );
+define( 'YM_NAME', 'iiifx-yandex-money-test.jelasticloud.com' );
+define( 'YM_SITE', 'https://iiifx-yandex-money-test.jelasticloud.com/' );
+define( 'YM_RESPONSE_LINK', 'https://iiifx-yandex-money-test.jelasticloud.com/oauth_response.php' );
+define( 'YM_CLIENT_ID', 'CF872D6B8C71EB5E5CFD79B82BC0514EA988E4E20525E44DBA1A4852700F7FE5' );
+define( 'YM_CLIENT_SECRET', 'A6D85B1C0B43C62AD93B62053BFF51819947034E046DEB2B2584B1A2710D7779820C90F3E01C806A21AD4AE4DD64262D36E4C2C4E39961E75B74359A2FE509E7' );
