6eb6a93a0abf99591bf008262eca64dc3b823b86
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2014-08-29 18:38:59 +0300

    no message

--- do/oauth_request.php
+++ do/oauth_request.php
@@ -1,5 +1,36 @@
 <?php
 
+/*
 echo '<pre>';
 var_export( $_POST );
 echo '</pre>';
+*/
+
+$rightsConfigurator = \YandexMoney\YandexMoney::getRightsConfigurator();
+
+$rightsConfigurator->addRight( \YandexMoney\Presets\Rights::ACCOUNT_INFO );
+$rightsConfigurator->addRight( \YandexMoney\Presets\Rights::OPERATION_HISTORY );
+//$rightsConfigurator->addRight( \YandexMoney\Presets\Rights::OPERATION_DETAILS );
+//$rightsConfigurator->addRight( \YandexMoney\Presets\Rights::PAYMENT_P2P );
+
+//$rightsConfigurator->paymentToAccount( $sellerPurse, PaymentIdentifier::ACCOUNT, 1, 10000 );
+//$rightsConfigurator->paymentToPattern( "337", 1, 10000 );
+//$rightsConfigurator->setMoneySource( MoneySource::WALLET );
+
+$authRequestBuilder = \YandexMoney\YandexMoney::getAuthRequestBuilder();
+$authRequestBuilder->setClientId( YM_CLIENT_ID );
+$authRequestBuilder->setRedirectUri( YM_RESPONSE_LINK );
+$authRequestBuilder->setRights( $rightsConfigurator->toString() );
+
+$apiFacade = \YandexMoney\YandexMoney::getApiFacade();
+
+$originalServerResponse = NULL;
+try {
+    $originalServerResponse = $apiFacade->authorizeApplication( $authRequestBuilder );
+} catch ( \Exception $e ) {
+    echo '[Error: ' . $e->getMessage() . ' ]';
+}
+
+if ( $originalServerResponse ) {
+    echo '<a href="' . $originalServerResponse->getHeader( 'Location' ) . '" target="_blank">Запрос oAuth на Yandex.Money</a>';
+}
