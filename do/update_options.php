9e658e3dce8863eec784fce810a46b9c0c64c682
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2014-09-01 10:34:01 +0300

    no message

---
+++ do/update_options.php
@@ -0,0 +1,16 @@
+<?php
+
+if ( $_POST ) {
+    if ( isset( $_POST[ 'sellerOptions.client-purse' ] ) && $_POST[ 'sellerOptions.client-purse' ] ) {
+        $_SESSION[ 'sellerOptions.client-purse' ] = $_POST[ 'sellerOptions.client-purse' ];
+    }
+    if ( isset( $_POST[ 'sellerOptions.response-link' ] ) && $_POST[ 'sellerOptions.response-link' ] ) {
+        $_SESSION[ 'sellerOptions.response-link' ] = $_POST[ 'sellerOptions.response-link' ];
+    }
+    if ( isset( $_POST[ 'sellerOptions.client-id' ] ) && $_POST[ 'sellerOptions.client-id' ] ) {
+        $_SESSION[ 'sellerOptions.client-id' ] = $_POST[ 'sellerOptions.client-id' ];
+    }
+    if ( isset( $_POST[ 'sellerOptions.client-secret' ] ) && $_POST[ 'sellerOptions.client-secret' ] ) {
+        $_SESSION[ 'sellerOptions.client-secret' ] = $_POST[ 'sellerOptions.client-secret' ];
+    }
+}
