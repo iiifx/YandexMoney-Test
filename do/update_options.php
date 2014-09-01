aa86cc1873ffb87dc3abe6d6b2dac4855efccbe1
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2014-09-01 10:41:16 +0300

    no message

--- do/update_options.php
+++ do/update_options.php
@@ -1,17 +1,17 @@
 <?php
 
 if ( $_POST ) {
-    if ( isset( $_POST[ 'sellerOptions.client-purse' ] ) && $_POST[ 'sellerOptions.client-purse' ] ) {
-        $_SESSION[ 'sellerOptions.client-purse' ] = $_POST[ 'sellerOptions.client-purse' ];
+    if ( isset( $_POST[ 'sellerOptions_client-purse' ] ) && $_POST[ 'sellerOptions_client-purse' ] ) {
+        $_SESSION[ 'sellerOptions_client-purse' ] = $_POST[ 'sellerOptions_client-purse' ];
     }
-    if ( isset( $_POST[ 'sellerOptions.response-link' ] ) && $_POST[ 'sellerOptions.response-link' ] ) {
-        $_SESSION[ 'sellerOptions.response-link' ] = $_POST[ 'sellerOptions.response-link' ];
+    if ( isset( $_POST[ 'sellerOptions_response-link' ] ) && $_POST[ 'sellerOptions_response-link' ] ) {
+        $_SESSION[ 'sellerOptions_response-link' ] = $_POST[ 'sellerOptions_response-link' ];
     }
-    if ( isset( $_POST[ 'sellerOptions.client-id' ] ) && $_POST[ 'sellerOptions.client-id' ] ) {
-        $_SESSION[ 'sellerOptions.client-id' ] = $_POST[ 'sellerOptions.client-id' ];
+    if ( isset( $_POST[ 'sellerOptions_client-id' ] ) && $_POST[ 'sellerOptions_client-id' ] ) {
+        $_SESSION[ 'sellerOptions_client-id' ] = $_POST[ 'sellerOptions_client-id' ];
     }
-    if ( isset( $_POST[ 'sellerOptions.client-secret' ] ) && $_POST[ 'sellerOptions.client-secret' ] ) {
-        $_SESSION[ 'sellerOptions.client-secret' ] = $_POST[ 'sellerOptions.client-secret' ];
+    if ( isset( $_POST[ 'sellerOptions_client-secret' ] ) && $_POST[ 'sellerOptions_client-secret' ] ) {
+        $_SESSION[ 'sellerOptions_client-secret' ] = $_POST[ 'sellerOptions_client-secret' ];
     }
 
     echo '<pre>';
